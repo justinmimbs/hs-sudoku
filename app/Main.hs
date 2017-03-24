61983bc34a036926653dd049ddccb41ad7011eee
Justin Mimbs <justin.mimbs@gmail.com>
2017-03-24 16:15:58 -0400

    Add valuesAssign and valuesEliminate

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,16 @@ module Main where
 import Sudoku
 
 
+example1 :: [ Char ]
 example1 =
     "003020600900305001001806400008102900700000008006708200002609500800203009005010300"
 
 
+example2 :: [ Char ]
 example2 =
     "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......"
 
 
 main :: IO ()
 main =
-    putStrLn (maybe "bad values" valuesToString (valuesFromString example1))
+    putStrLn (maybe "bad values" valuesToString (valuesFromString example2))
--- src/Sudoku.hs
+++ src/Sudoku.hs
@@ -1,8 +1,10 @@
 module Sudoku
     where
 
+import Control.Monad (foldM, (>=>))
 import Data.List (unlines, unwords, intercalate)
-import Data.Map (Map)
+import Data.Map (Map, (!))
+import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import Flow
 import qualified Data.Map as Map
@@ -78,25 +80,6 @@ type Values =
     Map Square [ Char ]
 
 
-valuesFromString :: String -> Maybe Values
-valuesFromString string =
-    let
-        blanks =
-            ".0"
-
-        chars =
-            string |> filter (\c -> elem c (blanks ++ digits))
-    in
-        if length chars == length squares then
-            chars
-                |> map (\c -> if elem c blanks then digits else [ c ])
-                |> zip squares
-                |> Map.fromList
-                |> Just
-        else
-            Nothing
-
-
 valuesToString :: Values -> String
 valuesToString values =
     let
@@ -113,6 +96,79 @@ valuesToString values =
             |> groupsOf 3
             |> intercalate [ replicate 3 (replicate (len * 3 + 2) '-') |> intercalate "-+-" ]
             |> unlines
+
+
+valuesFromString :: String -> Maybe Values
+valuesFromString string =
+    let
+        blanks =
+            ".0"
+
+        chars =
+            string |> filter (\c -> elem c (blanks ++ digits))
+    in
+        if length chars == length squares then
+            chars
+                |> zip squares
+                |> filter (\( _, char ) -> not (elem char blanks))
+                |> foldM
+                    (\values ( square, d ) -> valuesAssign square d values)
+                    valuesEmpty
+        else
+            Nothing
+
+
+valuesEmpty :: Values
+valuesEmpty =
+    replicate (length squares) digits
+        |> zip squares
+        |> Map.fromList
+
+
+valuesAssign :: Square -> Char -> Values -> Maybe Values
+valuesAssign square d values =
+    Map.lookup square values
+        |> maybe [] (filter (/= d))
+        |> foldM (flip (valuesEliminate square)) values
+
+
+valuesEliminate :: Square -> Char -> Values -> Maybe Values
+valuesEliminate square x values =
+    if not (elem x (values ! square)) then
+        Just values
+    else
+        case values ! square |> filter (/= x) of
+            [] ->
+                Nothing
+
+            value@(d : ds) ->
+                values
+                    |> Map.insert square value
+                    |> (if ds == [] then
+                            (\values ->
+                                peersBySquare ! square
+                                    |> foldM (\v s -> valuesEliminate s d v) values
+                            )
+                        else
+                            Just
+                       )
+                    >=> (\values ->
+                            unitsBySquare ! square
+                                |> foldM
+                                    (\v unit ->
+                                        case unit |> filter (\s -> elem x (v ! s)) of
+                                            [] ->
+                                                Nothing
+
+                                            s : [] ->
+                                                v |> valuesAssign s x
+
+                                            _ ->
+                                                Just v
+
+                                    )
+                                    values
+                        )
 
 
 -- utilities
